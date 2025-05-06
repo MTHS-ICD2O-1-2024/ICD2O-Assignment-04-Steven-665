983dbdaf5d1cce54180763b261db0b7b5a04792d
Yi-Steven-Zhou <yi.zhou@stu.ocsb.ca>
2025-05-06 17:29:37 +0000

    fixed js

--- js/script.js
+++ js/script.js
@@ -9,34 +9,41 @@
 
 
 // Setting the generate as function
-function generate() {
+function buyASushi() {
   // Setting the veribals
   const regularSize = document.getElementById("regular-size").checked
   const regularPrice = 5.99
   const bigPrice = 7.99
-  const Price = 0
+  const price = 0
 
-  const salmon = document.getElementById("salmon").checked
-  const tuna = document.getElementById("tuna")
-  const arcticClam = document.getElementById("arctic-clam")
-  const sweetShrimp = document.getElementById("sweet-shrimp")
-  const salmonPrice = 8.99
-  const tunaPrice = 9.99
-  const arcticClamPrice = 7.99
-  const sweetShrimpPrice = 6.99
-  const Price2 = 0
+  const sushiType = document.getElementById("Ingredients-of-sushi")
+  const price2 = 0
+  const salmonPrice = 6.99
+  const tunaPrice = 7.99
+  const arcticClamPrice = 6.99
+  const sweetShrimpPrice = 5.99
+  const tax = 0.13
 
   // Setting the the if and else statment
   if (regularSize === true) {
-    Price = regularPrice
+    price = regularPrice
   } else {
-    Price = bigPrice
+    price = bigPrice
   }
 
-  if () {
-    Price2 = salmonPrice
-  } else if 
+  if (sushiType = "salmon") {
+    price2 = salmonPrice
+  } else if (sushiType = "tuna") {
+    price2 = tunaPrice
+  } else if (sushiType = "arctic-clam") {
+    price2 = arcticClamPrice
+  } else if (sushiType = "sweet-shrimp") {
+    price2 = sweetShrimpPrice
+  } 
   // Gettting the reuslt
   document.getElementById("result").innerHTML =
-    "<p>The random number is: " + randomNumber + "</p>"
+    "<p>The Size Price is: " + price + "</p>"
+    "<p>The Food Material Price is: " + price2 + "</p>"
+    "<p>The Total Price is: " + (price + price2) + "</p>"
+    "<p>After Tax is: " + ((price + price2) * tax) + "</p>"
 }
